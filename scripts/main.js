80d46b4b2e8e05e43178408d13034bc92e9468ad
DoctorEenot <dreenot@gmail.com>
2021-11-10 03:46:07 +0200

    a bit of designing

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+function main() {
+
+    const BUTTONS_IN_MAIN_MENU = document.getElementById('main_menu_button_group');
+    var temp = BUTTONS_IN_MAIN_MENU.getBoundingClientRect();
+    const BUTTONS_IN_MAIN_MENU_HEIGHT = temp.height;
+    const BUTTONS_IN_MAIN_MENU_WIDTH = temp.width;
+
+    var
+    // Obtain a reference to the canvas element using its id.
+    game_window = document.getElementById('game_window'),
+    // Obtain a graphics context on the canvas element for drawing.
+    context = game_window.getContext('2d');
+   // Start listening to resize events and draw canvas.
+
+
+   var main_bg = document.getElementById('main_bg');
+   var online_button = document.getElementById('online_button');
+
+   var state = 0; // 0 - main menu
+   initialize();
+   
+
+   function initialize() {
+       // Register an event listener to call the resizeCanvas() function 
+       // each time the window is resized.
+       window.addEventListener('resize', resizeCanvas, false);
+       // Draw canvas border for the first time.
+       resizeCanvas();
+    }
+
+    function redraw_main_menu() {
+        context.drawImage(main_bg,0,0,
+                        game_window.width,game_window.height);
+
+        var new_height = (game_window.height - BUTTONS_IN_MAIN_MENU_HEIGHT)/2;
+        var new_width = (game_window.width - BUTTONS_IN_MAIN_MENU_WIDTH)/2;
+
+        BUTTONS_IN_MAIN_MENU.style.top = new_height;
+        BUTTONS_IN_MAIN_MENU.style.left = new_width;
+    }
+
+    // Runs each time the DOM window resize event fires.
+    // Resets the canvas dimensions to match window,
+    // then draws the new borders accordingly.
+    function resizeCanvas() {
+        game_window.width = window.innerWidth;
+        game_window.height = window.innerHeight;
+
+        switch(state){
+            case 0:
+                redraw_main_menu();
+                break;
+        }
+    }
+}
