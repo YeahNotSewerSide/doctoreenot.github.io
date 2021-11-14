1da1f427188d6bc38a2d6b88f5aeb6f225a4e011
DoctorEenot <dreenot@gmail.com>
2021-11-14 03:53:10 +0200

    added loggin in

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,12 @@
 
 
 function main() {
+    const SI_API_URL = "https://vladimirkhil.com/api/si";
+    const SI_API_USERAGENT = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
+    
+    // NO DATA IS SAVED, BYPASSING CORS ON SI GAME"S SERVER!!!!!
+    const PROXY = "https://floating-thicket-59406.herokuapp.com/"
+
     // Load sha1 module
     var webasm = 0;
     var WASM_LOADED = false;
@@ -48,13 +54,17 @@ function main() {
     var LMB_LOGIN = document.getElementById("login");
     var LMB_SET_PFP = document.getElementById("set_pfp");
     var LMB_BACK = document.getElementById("back_login");
-    var LMB_USERNAME = document.getElementById("username");
+    var LMI_USERNAME = document.getElementById("username");
+
 
     // set pfp menu
     const DRAGNDROP_ZONE_IN_SET_PFP_MENU = document.getElementById("set_pfp_group");
     var temp = DRAGNDROP_ZONE_IN_SET_PFP_MENU.getBoundingClientRect();
     const DRAGNDROP_ZONE_IN_SET_PFP_MENU_HEIGHT = temp.height;
     const DRAGNDROP_ZONE_IN_SET_PFP_MENU_WIDTH = temp.width;
+
+    var DRAGNDROP_ZONE_PFP = document.getElementById("pfp_d&d");
+    var DND_BACK = document.getElementById("back_pfp");
 
     // waiting screen
     const WAITING_MENU = document.getElementById("wait");
@@ -64,14 +74,21 @@ function main() {
     const WAIT_NOTICE = document.getElementById("wait_notice");
 
 
-    var DRAGNDROP_ZONE_PFP = document.getElementById("pfp_d&d");
-    var DND_BACK = document.getElementById("back_pfp");
-
-
     var last_resize = 0;
     var in_resize = false;
     var main_bg = document.getElementById('main_bg');
-    var reading_image = false;
+
+
+    // PROFILE PICTURE
+    var pfp_blob = 0;
+    var pfp_hash = 0;
+    var pfp_url = 0;
+    var pfp_filename = 0;
+
+    // LOGIN DATA
+    var USERNAME = 0;
+    var TOKEN = 0;
+    var SERVER_URL = 0;
 
     var stage = 0; // 0 - main menu
                     // 1 - set username/pfp
@@ -91,6 +108,7 @@ function main() {
         LMB_BACK.addEventListener('click',LMB_BACK_CLICK,false);
         LMB_SET_PFP.addEventListener('click',LMB_SET_PFP_CLICK,false);
         DND_BACK.addEventListener('click',DND_BACK_CLICK,false);
+        LMB_LOGIN.addEventListener('click',LMB_LOGIN_CLICK,false);
 
         // Draw canvas border for the first time.
         resizeCanvas_wrapped();
@@ -188,14 +206,14 @@ function main() {
             LMB_LOGIN.removeAttribute("disabled");
             LMB_SET_PFP.removeAttribute("disabled");
             LMB_BACK.removeAttribute("disabled");
-            LMB_USERNAME.removeAttribute("disabled");
+            LMI_USERNAME.removeAttribute("disabled");
             BUTTONS_IN_LOGIN_MENU.removeAttribute("style");
         }
         function disable_login_menu(){
             LMB_LOGIN.setAttribute("disabled","1");
             LMB_SET_PFP.setAttribute("disabled","1");
             LMB_BACK.setAttribute("disabled","1");
-            LMB_USERNAME.setAttribute("disabled","1");
+            LMI_USERNAME.setAttribute("disabled","1");
             BUTTONS_IN_LOGIN_MENU.setAttribute("style","z-index:0;display:none;");
         }
 
@@ -215,10 +233,11 @@ function main() {
 
         function enable_waiting_menu(wait_notice){
             WAIT_NOTICE.innerText = wait_notice;
+            WAITING_MENU.removeAttribute('style');
             var temp = WAITING_MENU.getBoundingClientRect();
             WAITING_MENU_HEIGHT = temp.height;
             WAITING_MENU_WIDTH = temp.width;
-            WAITING_MENU.removeAttribute('style');
+            
         }
         function disable_waiting_menu(){
             WAITING_MENU.setAttribute('style',"z-index:0;display:none;");
@@ -227,8 +246,11 @@ function main() {
         // HANDLERS
 
         function PFP_SET_DROP_HANDLER_ON_FINISH(e){
+            // LOADING IMAGE
             var img_data = e.target.result;
             console.log("finished reading");
+
+            pfp_blob = new Blob([img_data]);
             
             var bytes = new Uint8Array(img_data);
 
@@ -242,16 +264,19 @@ function main() {
             for(var i=0; i<len; i++){
                 binary += String.fromCharCode(hash_bytes[i]);
             }
-            var hash_base64 = window.btoa(binary);
-            console.log(hash_base64);
+            pfp_hash = window.btoa(binary);
+            console.log(pfp_hash);
 
             in_resize = true;
             disable_waiting_menu();
             enable_pfp_set_menu();
             redraw_set_pfp_menu();
             in_resize = false;
+            stage = 2;
+
         }
         function PFP_SET_DROP_HANDLER(event){
+            // LOADING IMAGE
             event.stopPropagation();
             event.preventDefault();
 
@@ -272,12 +297,20 @@ function main() {
                 alert("That file is not an image!");
                 return;
             }
+            console.log(file.size);
+            if(file.size>1048576){
+                alert("Размер файла не должен превышать 1мб!");
+                return;
+            }
 
+            pfp_filename = file.name;
             var reader = new FileReader();
             reading_image = true;
             var img_data = 0;
             reader.addEventListener('load',(e)=>{PFP_SET_DROP_HANDLER_ON_FINISH(e)});
             reader.readAsArrayBuffer(file);
+
+            stage = 3;
 
             in_resize = true;
             disable_pfp_set_menu();
@@ -327,6 +360,91 @@ function main() {
             stage = 1;
             in_resize = false;
         }
+
+        async function LMB_LOGIN_CLICK(){
+
+            if(LMI_USERNAME.value === ''){
+                alert('Введите никнейм!');
+                return;
+            }
+
+            // set username
+            USERNAME = LMI_USERNAME.value;
+
+            in_resize = true;
+            disable_login_menu();
+            enable_waiting_menu("Выполняется вход...");
+            redraw_waiting_menu();
+            stage = 3;
+            in_resize = false;
+
+            // getting server uri
+            var response = await fetch(SI_API_URL+"/servers");
+            var response_decoded = await response.json();
+            SERVER_URL = response_decoded[0].uri;
+            console.log(SERVER_URL);
+
+            // logging in
+            response = await fetch(PROXY+SERVER_URL+"/api/Account/LogOn",{
+                credentials: "include",
+                "headers": {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0",
+                    "Accept": "*/*",
+                    "Accept-Language": "en-US,en;q=0.5",
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    'Connection': 'keep-alive',
+                    'Accept-Encoding': 'gzip, deflate',
+                },
+                "method": "POST",
+                "mode": "cors",
+                'body':'login='+USERNAME+'&password='
+            });
+
+            if(response.status === 403){
+                alert("Это имя запрещено на сервере!");
+                in_resize = true;
+                disable_waiting_menu();
+                enable_login_menu();
+                redraw_login_menu();
+                stage = 2;
+                in_resize = false;
+                return;
+            }
+            if(response.status === 409){
+                alert("Это имя занято!");
+                in_resize = true;
+                disable_waiting_menu();
+                enable_login_menu();
+                redraw_login_menu();
+                stage = 2;
+                in_resize = false;
+                return;
+            }
+            
+            TOKEN = await response.text();
+            console.log('token: ',TOKEN);
+
+            // uploading image
+            if(pfp_blob !== 0){
+                console.log("uploading image");
+                var data = new FormData();
+                var file = new File([pfp_blob],pfp_filename);
+                data.append(pfp_hash,file);
+
+                response = await fetch(PROXY+SERVER_URL+"/api/upload/image",{
+                    credentials: "include",
+                    "method":"POST",
+                    "body":data,
+                    "headers":{"Content-MD5":pfp_hash}
+                })
+                pfp_url = await response.text;
+                console.log(pfp_url);
+            }else{
+                console.log("no image is set");
+            }
+
+        }
+
 
 }
 
